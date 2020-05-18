67085e51570df7f74f8280188659caec1a2fdae6
AJ Guiles <ashiguiles@gmail.com>
2020-05-18 03:06:23 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 // create your App component here
+class App extends Component {
 
-class App extends component {
-  state = {
-    peepleInSpace: = []
+    state = {
+        peopleInSpace: []
+    }
+  
+    render() {
+      return (
+        <div>
+          {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+        </div>
+      )
+    }
+  
+    componentDidMount() {
+      fetch('http://api.open-notify.org/astros.json')
+        .then(response => response.json())
+        .then(({people}) => this.setState({ peopleInSpace: people }))
+  
+    }
+  
   }
   
-  render() {
-    <div>
-    {this.state.peopleInSpace.map((person, id) => <h1 key ={id}>{person.name}<h1)>
-  }
-}
+  export default App
