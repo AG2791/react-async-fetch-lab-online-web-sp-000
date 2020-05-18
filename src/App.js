c91dab8dbb5f6e50b900e0b113228d8029284765
AJ Guiles <ashiguiles@gmail.com>
2020-05-18 03:04:26 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
 // create your App component here
+
+class App extends component {
+  state = {
+    peepleInSpace: = []
+  }
+  
+  render() {
+    <div>
+    {this.state.peopleInSpace.map
+  }
+}
